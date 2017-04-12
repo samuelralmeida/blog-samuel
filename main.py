1f81523f45911782a5d76860ee0e5978eeb76fd3
Samuel Almeida <samuel.ralmeida@gmail.com>
2017-04-12 14:34:42 -0300

    change file names

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
-secret = 'sars'
+secret = 'sras'
 
 
 def render_str(template, **params):
